adb3a79f7e1eae7b229b44f9ab33204ece902fea
ysmnababan <ysmnababan@students.hacktiv8.ac.id>
2025-11-17 12:57:12 +0700

    Fix write error handling in benchmark tests for direct and buffered writes

--- buffiobm/buffio_vs_direct_test.go
+++ buffiobm/buffio_vs_direct_test.go
@@ -12,7 +12,7 @@ func BenchmarkDirectWrite(b *testing.B) {
 	for i := 0; i < b.N; i++ {
 		f, _ := os.Create("direct.txt")
 		for j := 0; j < iterations; j++ {
-			f.Write([]byte("x"))
+			_, _ = f.Write([]byte("x"))
 		}
 		f.Close()
 	}
@@ -23,9 +23,23 @@ func BenchmarkBufioWrite(b *testing.B) {
 		f, _ := os.Create("bufio.txt")
 		w := bufio.NewWriter(f)
 		for j := 0; j < iterations; j++ {
-			w.Write([]byte("x"))
+			_, _ = w.Write([]byte("x"))
 		}
 		w.Flush()
 		f.Close()
 	}
 }
+
+/*
+go test -bench=.
+
+goos: linux
+goarch: amd64
+pkg: buffiobm
+cpu: Intel(R) Core(TM) i5-10310U CPU @ 1.70GHz
+BenchmarkDirectWrite-8               133           9157738 ns/op
+BenchmarkBufioWrite-8               5072            211429 ns/op
+PASS
+ok      buffiobm        3.225s
+
+*/
